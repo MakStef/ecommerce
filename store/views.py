e1388b5eea2bfeff06172d806204388f986c1da6
MakStef <stevaniwwasil@gmail.com>
2022-09-27 16:57:48 +0200

    Basic settings, apps, urls

--- store/views.py
+++ store/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import (
+    View,
+    TemplateView,
+)
+from django.views.generic.base import (
+    ContextMixin,
+)
+
+# Mixins
+
 
 # Create your views here.
+class HomepageView(View, ContextMixin):
+    template_name = "store/homepage.html"
+    extra_context = {
+    }
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
